c46db3f25868e5fa72d35f2e6133808dc45f7f13
zwang7 <50262510+Kieachernar@users.noreply.github.com>
2021-05-04 09:56:23 +0100

    Update svm.py

--- svm.py
+++ svm.py
@@ -18,21 +18,16 @@ train_vectors = vectorizer.fit_transform(trainData['Content'])
 test_vectors = vectorizer.transform(testData['Content'])
 # Perform classification with SVM, kernel=linear
 classifier_linear = svm.SVC(kernel='linear')
-t0 = time.time()
 classifier_linear.fit(train_vectors, trainData['Label'])
-t1 = time.time()
 prediction_linear = classifier_linear.predict(test_vectors)
-t2 = time.time()
-time_linear_train = t1-t0
-time_linear_predict = t2-t1
+
 # results
-# print("Training time: %fs; Prediction time: %fs" % (time_linear_train, time_linear_predict))
 report = classification_report(testData['Label'], prediction_linear, output_dict=True)
 print('positive: ', report['pos'])
 print('negative: ', report['neg'])
 print('accuracy of the model: ', report['accuracy'])
 
-testinput = "I'd say this humidifier is okayish"
-testinput_vector = vectorizer.transform([testinput]) # vectorizing
+testinput = "I'd say this humidifier is okayish" #can customise sentence here
+testinput_vector = vectorizer.transform([testinput])
 print('customised input:',testinput)
 print('prediction: ',classifier_linear.predict(testinput_vector))
